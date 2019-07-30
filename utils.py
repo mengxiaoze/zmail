6045002239ba98abe7d826e55ae01f4b445602e8
OriginZero <zhangqingde12138@outlook.com>
2019-07-30 11:07:08 +0800

    添加utils工具包

---
+++ utils.py
@@ -0,0 +1,21 @@
+import os
+from config import BASE_DIR, RECIPIENT_PATH
+
+
+def parser_recipient():
+    try:
+        with open(RECIPIENT_PATH, 'r') as f:
+            lines = [_.strip() + '@qq.com' for _ in f.readlines()]
+    except PermissionError as e:
+        print('路径不正确，重新配置config文件\'RECIPIENT_PATH\'')
+        print(e)
+    except FileNotFoundError as e:
+        print('文件不存在!')
+        print(e)
+    except Exception as e:
+        print(e)
+    return lines
+
+
+def parser_account():
+    pass
